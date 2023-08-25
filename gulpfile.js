b35432f773ebf65b8527105a580ed93517eb4ba4
Roman Katrovsky <katrovsky@gmail.com>
2023-08-25 12:05:37 +0300

    правит watcher

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload());
+  gulp.watch("source/*.html").on("change", () => sync.reload());
 }
 
 
